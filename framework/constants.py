c80bd4df73393cc96694e15bdb3a0a4bd971f1a8
SHIVAM LOHAKARE <64030329+ShivamLohakare@users.noreply.github.com>
2026-06-05 10:54:07 +0530

    Update constants.py

--- framework/constants.py
+++ framework/constants.py
@@ -3,3 +3,7 @@ SUPPORTED_FILE_FORMATS = {"csv", "parquet", "delta"}
 SUPPORTED_WRITE_MODES = {"overwrite", "append", "merge"}
 SUPPORTED_STATUSES = {"STARTED", "SUCCESS", "FAILED", "SKIPPED"}
 MANDATORY_FIELDS = {"dataset_name", "source_type", "target_catalog", "target_schema", "target_table", "write_mode", "active_flag"}
+DEPENDENCY_RULES = { "file" : ["source_path", "file_format"],
+                     "sqlserver" : ["server", "database", "table"],
+                     "snowflake" : ["account", "warehouse", "database", "schema", "table"],
+                     "api" : ["base_url", "endpoint"]}
